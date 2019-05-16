5ebcb2db7104f99c90d2ba526349a152aa7ad7ed
2oo5mc <31730375+2oo5mc@users.noreply.github.com>
2019-05-16 21:21:45 +0900

    Beautify

--- winpause.c
+++ winpause.c
@@ -1,15 +1,14 @@
-
 #include <stdio.h>
+
 #include <stdlib.h>
 
-int main(){
-printf("wait for press enter key\n");
+int main() {
+  printf("wait for press enter key\n");
 
-while (1) {
-        getchar();
+  while (1) {
+    getchar();
     exit(0);
-}
+  }
 
-
-return 0;
+  return 0;
 }
